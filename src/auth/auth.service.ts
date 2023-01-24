ce814a1067fba175c133484c1dcc20675dffbeba
Ana Neri <ananeri32@hotmail.com>
2023-01-24 18:38:26 -0300

    feat(service, prisma): adding logic signin and signout and ajuste schemas
    Signed-off-by: Ana Neri <ananeri32@hotmail.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,51 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { AuthDto } from "src/dto";
 import { PrismaService } from "src/prisma/prisma.service";
+import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
     
-    signup(dto: AuthDto) {
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+        try {
+          const user = await this.prisma.user.create({
+            data: {
+              email: dto.email,
+              hash,
+            },
+          });
+    
+          delete user.hash;
 
-        return { mgs: 'I have sign up' }
+          return user;
+        } catch (error) {
+          if ( error instanceof PrismaClientKnownRequestError ) {
+            if (error.code === 'P2002') {
+              throw new ForbiddenException('Credentials taken',);
+            }
+          }
+          throw error;
+        }
+      }
+
+      async signin(dto: AuthDto) {
+
+        const user =
+          await this.prisma.user.findUnique({
+            where: {
+              email: dto.email,
+            },
+          });
+
+        if(!user) throw new ForbiddenException ('Credentials incorret', );
+
+
     }
 
-    signin() {        console.log({
-        dto,
-    })
-
-        return { mgs: 'I have sign in' }
-
-    }
+        
 }
 
--- src/dto/auth.dto.ts
+++ src/dto/auth.dto.ts
@@ -1,13 +1,11 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator"
 
 export class AuthDto {
-
     @IsEmail()
     @IsNotEmpty()
-    email:  string
-
+    email: string;
+  
     @IsString()
     @IsNotEmpty()
-    password: string
-
-}
+    password: string;
+  }
