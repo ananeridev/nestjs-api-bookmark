c2e44b3721cc4f17525da4da68a847b50f3c91c5
Ana Neri <ananeri32@hotmail.com>
2023-02-05 12:06:26 -0300

    solving 'jwt unkkonwn authentication strategy' error
    Signed-off-by: Ana Neri <ananeri32@hotmail.com>

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy';
 
 @Module({
     imports: [JwtModule.register({})],
     controllers: [AuthController],
-    providers: [AuthService],
-})
-export class AuthModule {}
+    providers: [AuthService, JwtStrategy],
+  })
+  export class AuthModule {}
